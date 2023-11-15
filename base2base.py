4939698ad74925e72788eda5674b36eebba900d2
rockycape <19771537+rockycape@users.noreply.github.com>
2023-11-15 15:06:04 +1100

    update

--- base2base.py
+++ base2base.py
@@ -1,5 +1,5 @@
 # If you specify the base of the input number, you can use 
-# the function int(number_input, base) to ouput the number in decimal. 
+# the function int(number_input, base) which ouputs the number in decimal. 
 print('This program converts a number from any base to decimal')
 print('What is the base of the number?')
 b = int(input())  # Convert to integer
@@ -7,5 +7,3 @@ print('What is the number?')
 ni = input()
 decimal_number = int(ni, b)
 print(f"The decimal representation of {ni} in base {b} is: {decimal_number}")
-
-

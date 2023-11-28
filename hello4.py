1781de25202ee4632b8e78ad4fad662d3fa4815d
rockycape <19771537+rockycape@users.noreply.github.com>
2023-11-28 16:30:55 +1100

    update

--- hello4.py
+++ hello4.py
@@ -2,7 +2,7 @@ from typing import Annotated, Dict, List, Literal, Tuple
 
 from annotated_types import Gt
 
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, EmailStr, field_validator
 
 class User(BaseModel):
     name: str
